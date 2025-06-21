47858105cfe43af855c8ea19dba5da2682123a8d
aym0546 <ciany.f@gmail.com>
2025-06-22 00:11:21 +0900

    Add profile page with dynamic data and header link

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
     resources :comments, only: [:new, :create]
   end
 
+  resource :profile, only: [:show, :edit, :update]
+   # userに対してindexは1つなのでindexは不要
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
