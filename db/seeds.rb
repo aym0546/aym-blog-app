3b1fa70decbaeaa5b65facfb50fa56b221eca594
aym0546 <ciany.f@gmail.com>
2025-06-21 17:14:27 +0900

    Update seeds file to reflect user-article association

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,18 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-10.times do
-    Article.create(
+john = User.create!(email: 'john@examile.com', password: 'password')
+emily = User.create!(email: 'emily@examile.com', password: 'password')
+
+5.times do
+    john.articles.create(
+        title: Faker::Lorem.sentence(word_count: 5),
+        content: Faker::Lorem.sentence(word_count: 100)
+    )
+end
+
+5.times do
+    emily.articles.create(
         title: Faker::Lorem.sentence(word_count: 5),
         content: Faker::Lorem.sentence(word_count: 100)
     )
