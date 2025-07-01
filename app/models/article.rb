499ec2734d0da6211111d4e46b0fe40bca9d2d96
aym0546 <ciany.f@gmail.com>
2025-07-01 18:16:50 +0900

    feat: Display rich text editor in post form

--- app/models/article.rb
+++ app/models/article.rb
@@ -22,11 +22,6 @@ class Article < ApplicationRecord
     validates :title, format: { with: /\A(?!\@)/ } # 先頭が@は許可しない
 
     validates :content, presence: true
-    validates :content, length: { minimum: 10 }
-    validates :content, uniqueness: true
-
-    # title + content が 合計100文字以上ないと許可しない
-    validate :validate_title_and_content_length
 
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
@@ -44,9 +39,4 @@ class Article < ApplicationRecord
         likes.count
     end
 
-    private
-    def validate_title_and_content_length
-        char_count = self.title.length + self.content.length
-        errors.add(:content, '100文字以上で！！') unless char_count > 100 # contentカラムにエラーをadd
-    end
 end
