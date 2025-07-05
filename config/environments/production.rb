cbc7c1ce9170dc3ab9f8b929360e762a00e05772
aym0546 <ciany.f@gmail.com>
2025-07-05 23:00:09 +0900

    chore: configure ActionMailer with Mailgun SMTP settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,20 @@ Rails.application.configure do
   #   authentication: :plain
   # }
 
+  # 送信者のメールアドレスのドメイン（仮）
+  config.action_mailer.default_url_options = { host: 'example.com' }
+
+  # Mailgun
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    address:        ENV['MAILGUN_SMTP_SERVER'],
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         ENV['HEROKU_DOMAIN'],
+    authentication: :plain,
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
