5a248f6ece63a98abdba8d47fc7404e420b3e2f1
aym0546 <ciany.f@gmail.com>
2025-07-06 00:02:02 +0900

    chore: update ActionMailer host to Heroku domain for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: 'example.com' }
+  config.action_mailer.default_url_options = { host: 'aym-blog-app-32f1f970315f.herokuapp.com' }
 
   # Mailgun
   config.action_mailer.delivery_method = :smtp
