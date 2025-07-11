def520054cefcc3cc4defd062d6abf332e88fe24
aym0546 <ciany.f@gmail.com>
2025-07-11 10:35:26 +0900

    Refactor routes to namespace API endpoints under /api

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,16 @@
 require 'sidekiq/web'
-
 Rails.application.routes.draw do
 
   mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
 
   root to: 'articles#index'
 
   resource :timeline, only: [:show]
 
-  resources :articles do
-    resources :comments, only: [:index, :new, :create]
-
-    resource :like, only: [:show, :create, :destroy]
-  end
+  resources :articles
 
   resources :accounts, only: [:show] do
     resources :follows, only: [:create]
@@ -28,13 +18,14 @@ Rails.application.routes.draw do
   end
 
   resource :profile, only: [:show, :edit, :update]
-   # userに対してindexは1つなのでindexは不要
+
   resources :favorites, only: [:index]
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  namespace :api, defaults: {format: :json} do
+    scope '/articles/:article_id' do
+      resources :comments, only: [:index, :create]
+      resource :like, only: [:show, :create, :destroy]
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
