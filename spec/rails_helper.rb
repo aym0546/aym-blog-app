af2560e63fd0a7ff46db4f82dc1709b08cbac4e6
aym0546 <ciany.f@gmail.com>
2025-07-16 14:10:51 +0900

    Remove webdrivers and configure system specs to use selenium_chrome directly

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,7 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
@@ -72,4 +73,9 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
+
+  # System specで Selenium（GUI） + Chrome を使用する設定
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome
+  end
 end
